7fe933da13556f8629a6a6880b481cabcf6b2499
Dmitry Terentev <zabey52@gmail.com>
2022-11-20 16:31:25 +0300

    Добавление задачи 2

---
+++ 02/Lesson2_2__struct.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+// Задача 2. Счета
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // Корректное отображение Кириллицы
+    system("chcp 1251");
+
+
+
+
+
+
+
+}
