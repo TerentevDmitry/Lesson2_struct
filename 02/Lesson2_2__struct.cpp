ff574c3eca86a3ba297e153249c27f4f91e46e4f
Dmitry Terentev <zabey52@gmail.com>
2022-11-20 21:39:25 +0300

    Задача 2. Вар. 1

--- 02/Lesson2_2__struct.cpp
+++ 02/Lesson2_2__struct.cpp
@@ -1,25 +1,46 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 
 // Задача 2. Счета
 
+struct bankАccount
+{
+    int numberAccount = 0;
+    std::string nameOwner;
+    double moneyInAccount = 0.00;
+};
 
-
-
-
+void changingAccountBalance (bankАccount* person1, double newMoneyInAccount)
+{
+    person1->moneyInAccount = newMoneyInAccount;
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian"); // Корректное отображение Кириллицы
     system("chcp 1251");
 
+    bankАccount person1;
+    double newMoneyInAccount = 0;
 
+    std::cout << "Введите номер счёта: ";
+    std::cin >> person1.numberAccount;
+    
+    std::cout << "Введите имя владельца: ";
+    std::cin >> person1.nameOwner;
+    
+    std::cout << "Введите баланс: ";
+    std::cin >> person1.moneyInAccount;
+    
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newMoneyInAccount;
 
+    changingAccountBalance(&person1, newMoneyInAccount);
 
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
+    std::cout.precision(0);
+    std::cout << "Ваш счёт: ";
+    std::cout << person1.nameOwner << ", "
+              << person1.numberAccount << ", "
+              << std::fixed
+              << person1.moneyInAccount << " руб." 
+              << std::endl;
 }
