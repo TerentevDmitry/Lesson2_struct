e05569fb681fe2bea0ebe7416b43fe0763b5b15c
Dmitry Terentev <zabey52@gmail.com>
2022-11-20 13:10:30 +0300

    Добавьте файлы проекта.

---
+++ 01/Lesson2_1__struct.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+
+enum monthOfYear
+{
+    Январь = 1, Февраль, Март, Апрель, Май, Июнь, Июльь, Август, Сентябрь, Октябрь, Ноябрь, Декабрь
+};
+
+enum comment
+{
+    "До свидания" = -3;
+};
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // Корректное отображение Кириллицы
+    system("chcp 1251");
+
+
+    /*color paint = color_black;
+    color house(color_red);
+    color apple = color_red;
+
+    std::cout << paint << std::endl;
+    std::cout << house << std::endl;
+    std::cout << apple << std::endl;
+   
+    switch (apple)
+    {
+    case color_black: std::cout << "Чёрный" << std::endl;
+        break;
+    case color_red: std::cout << "Красный" << std::endl;
+        break;
+    case color_blue: std::cout << "Синий" << std::endl;
+        break;
+    default: std::cout << "Не угадал" << std::endl;
+        break;
+    }*/
+    
+
+
+
+    
+
+
+
+
+    //person p;
+    //std::cout << "name: ";
+    //p.first_name = "Егор";
+    ////std::cin >> p.first_name;
+    ////std::cout << std::endl;
+    //std::cout << p.first_name << std::endl;
+
+
+
+    //std::cout << "suname: ";
+    //p.second_name = "Летов";
+    ////std::cin >> p.second_name;
+    ////std::cout << std::endl;
+    //std::cout << p.second_name << std::endl;
+
+
+    //std::cout << "age: ";
+    //p.age = 39;
+    ////std::cin >> p.age;
+    ////std::cout << std::endl;
+
+    //std::cout << p.age << " years." << std::endl;
+
+
+}
